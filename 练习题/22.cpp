8c310545af265256841eaecc0d1483c585c1f0f8
李泽 <30529058+lize1516@users.noreply.github.com>
2018-06-04 23:01:35 +0800

    Create 22.cpp

---
+++ 练习题/22.cpp
@@ -0,0 +1,20 @@
+ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+        if(pListHead==NULL)
+            return NULL;
+    ListNode* first = pListHead;
+	ListNode* slow = pListHead;
+	while (--k)
+	{
+		if (first == NULL)
+			return NULL;
+		first = first->next;
+	}
+	if (first == NULL)
+		return NULL;
+	while (first->next)
+	{
+        first=first->next;
+		slow = slow->next;
+	}
+	return slow;
+    }
