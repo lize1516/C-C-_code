731774f58856076c992a358badd2d2b35abe7131
李泽 <30529058+lize1516@users.noreply.github.com>
2018-08-09 23:32:32 +0800

    Update day08(1).cpp

--- 每日一题/day08(1).cpp
+++ 每日一题/day08(1).cpp
@@ -1,4 +1,4 @@
-//删除倒数第k个节点
+//1.删除倒数第k个节点
 typedef struct ListNode
 {
 	int data;
@@ -27,4 +27,43 @@ ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
 		}
 	}
 	return slow;
+}
+
+//2.最大连续子数组之和
+void FindConnectMaxSum(vector<int> arr)
+{
+	int size = arr.size();
+	vector<int> str;
+	int sum=0;
+	str.resize(size);
+
+	for (int i = 0; i < size; i++)
+	{
+		sum = sum + arr[i];
+		str[i] = sum;
+		if (sum <= 0)
+			sum = 0;
+	}
+	int max = str[0];
+	for (int m = 0; m < size; m++)
+	{
+		if (max < str[m])
+			max = str[m];
+	}
+	cout << max << endl;
+}
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> arr;
+	arr.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	FindConnectMaxSum(arr);
+
+	system("pause");
+	return 0;
 }
